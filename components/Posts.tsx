0f592f6e971ba0681c2b36f79af55cfd3859972c
Nikita <khakimov.nikita.vch@gmail.com>
2023-07-09 17:50:29 +0300

    useApiRoute

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,24 +1,25 @@
 "use client";
-import useSWR from 'swr';
+import useSWR from "swr";
 import Link from "next/link";
-import { getAllPosts } from '@/services/getPosts';
+import { getAllPosts } from "@/services/getPosts";
 
 export const Posts = () => {
-
-	const {data: posts, isLoading} = useSWR('posts', getAllPosts)
-
+  const { data: posts, isLoading } = useSWR("posts", getAllPosts);
   return (
     <>
-      {isLoading ? <h1>Loading</h1> : ""}
-      <ul>
-        {posts.map((post: any) => {
-          return (
-            <li key={post.id}>
-              <Link href={`/blog/${post.id}`}>{post.title}</Link>
-            </li>
-          );
-        })}
-      </ul>
+      {isLoading ? (
+        <h1>Loading...</h1>
+      ) : (
+        <ul>
+          {posts.map((post: any) => {
+            return (
+              <li key={post.id}>
+                <Link href={`/blog/${post.id}`}>{post.title}</Link>
+              </li>
+            );
+          })}
+        </ul>
+      )}
     </>
   );
 };
